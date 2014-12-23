4a435bbac1281313b1f0c1b344a86f7ef82a6d0c
Santu Mahapatra <santu.m@multunus.com>
2014-12-23 16:19:29 +0530

    Finding out data from the json

--- src/application.js
+++ src/application.js
@@ -1,17 +1,22 @@
 var createQuestionTypes = function(data){
-  console.log(data);
+  var valuesForQuestionTypes = data;
+  entryList = data.feed.entry;
+  var entrylength = entryList.length;
+  for (var i =0; i < entrylength; i++){
+    console.log(entryList[i].gs$cell.$t);
+  }
 }
 
 var createRoles= function(data){
-  console.log(data);  
+  // console.log(data);
 }
 
 var createQuestions= function(data){
-  console.log(data); 
+  // console.log(data);
 }
 
 var createChoices = function(data){
-  console.log(data);
+  // console.log(data);
 }
 
 
