ffe80f9a4f7664bacdec4d3693b88466efcdf5d9
Santu Mahapatra <santu.m@multunus.com>
2014-12-23 15:18:30 +0530

    Getting Json from Google

---
+++ src/application.js
@@ -0,0 +1,20 @@
+var createQuestionTypes = function(data){
+  console.log(data);
+}
+
+var createRoles= function(data){
+  console.log(data);  
+}
+
+var createQuestions= function(data){
+  console.log(data); 
+}
+
+var createChoices = function(data){
+  console.log(data);
+}
+
+
+$(document).ready( function(){
+
+});
