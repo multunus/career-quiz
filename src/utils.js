07a6396169628b1746ee6313b3d6e75ef09c1a0d
Santu Mahapatra <santu.m@multunus.com>
2014-12-24 11:48:49 +0530

    Refactoring changes in createObject function

--- src/utils.js
+++ src/utils.js
@@ -3,33 +3,28 @@ utils = {
     EngineNameSpace[listType].push(givenObject);
   },
   createObject : function(objectType, argsList){
+    var q = null;
     switch( objectType ){
       case 'questionType':
         q = new questionType();
         q.initialize(argsList[0], argsList[1]);
-        return q;
         break;
 
       case 'question':
         q = new question();
         q.initialize(argsList[0], argsList[1], argsList[2]);
-        return q;
         break;
 
       case 'role':
         q = new role();
         q.initialize(argsList[0], argsList[1], argsList[2], argsList[3]);
-        return q;
         break;
 
       case 'choice':
         q = new choice();
         q.initialize(argsList[0], argsList[1], argsList[2], argsList[3]);
-        return q;
         break;
-
-      default:
-        return null;
     }
+    return q;
   }
 }
