30d1347741e897e2e126178710b75ac7c42725e0
Santu Mahapatra <santu.m@multunus.com>
2014-12-24 11:21:11 +0530

    Creating the utils createObject function.

--- spec/utilsSpec.js
+++ spec/utilsSpec.js
@@ -5,4 +5,15 @@ describe("utils", function(){
       expect(EngineNameSpace['listOfQuestionTypes']).toEqual(["a"]);
     });
   });
+
+  describe("createObject", function(){
+    it("creates questionType objects given a list of arguments", function(){
+      var argsList = [1, 'Profession']
+      var q = new questionType();
+      q.initialize(argsList[0], argsList[1]);
+      r = utils.createObject('questionType', argsList);
+      expect(q['pid']).toBe(r['pid']);
+      expect(q['ptype']).toBe(r['ptype']);
+    });
+  });
 });
--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,17 @@
 utils = {
   pushObjectToList : function(listType, givenObject){
     EngineNameSpace[listType].push(givenObject);
+  },
+  createObject : function(objectType, argsList){
+    switch( objectType ){
+      case 'questionType':
+        q = new questionType();
+        q.initialize(argsList[0], argsList[1])
+        return q;
+        break;
+
+      default:
+        return null;
+    }
   }
 }
