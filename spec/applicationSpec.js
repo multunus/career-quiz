f41e0eb46ea0629c92a8f60ebe8ac989ae41f69a
Santu Mahapatra <santu.m@multunus.com>
2014-12-24 15:15:31 +0530

    Adding code for createQuestions and createChoices funcrtions in the application.js

--- spec/applicationSpec.js
+++ spec/applicationSpec.js
@@ -45,12 +45,59 @@ describe("application", function(){
       }
     };
     
-    it("should create a list of objects in listOfQuestionTypes", function(){
+    it("should create a list of objects in listOfRoles", function(){
       createRoles(data);
       expect(EngineNameSpace['listOfRoles'][0]['pid']).toEqual('1');
       expect(EngineNameSpace['listOfRoles'][0]['questionTypeId']).toEqual('1');
       expect(EngineNameSpace['listOfRoles'][0]['roleName']).toEqual('programmer');
       expect(EngineNameSpace['listOfRoles'][0]['roleText']).toEqual('Programmer.');
+    });
+  });
+
+  describe("createQuestions", function(){
+    var data = {
+      feed: {
+        entry : [
+          { gs$cell : { $t : 'pid' } },
+          { gs$cell : { $t : 'questionTypeId' } },
+          { gs$cell : { $t : 'questionText' } },
+          { gs$cell : { $t : '1' } },
+          { gs$cell : { $t : '1' } },
+          { gs$cell : { $t : 'How do you spend your time?' } }
+        ]
+      }
+    };
+    
+    it("should create a list of objects in listOfQuestions", function(){
+      createQuestions(data);
+      expect(EngineNameSpace['listOfQuestions'][0]['pid']).toEqual('1');
+      expect(EngineNameSpace['listOfQuestions'][0]['questionTypeId']).toEqual('1');
+      expect(EngineNameSpace['listOfQuestions'][0]['questionText']).toEqual('How do you spend your time?');
+    });
+  });
+
+  describe("createChoices", function(){
+    var data = {
+      feed: {
+        entry : [
+          { gs$cell : { $t : 'pid' } },
+          { gs$cell : { $t : 'questionId' } },
+          { gs$cell : { $t : 'roleId' } },
+          { gs$cell : { $t : 'choiceText' } },
+          { gs$cell : { $t : '1' } },
+          { gs$cell : { $t : '1' } },
+          { gs$cell : { $t : '1' } },
+          { gs$cell : { $t : 'You enjoy finding solutions to problems.' } }
+        ]
+      }
+    };
+    
+    it("should create a list of objects in listOfChoices", function(){
+      createChoices(data);
+      expect(EngineNameSpace['listOfChoices'][0]['pid']).toEqual('1');
+      expect(EngineNameSpace['listOfChoices'][0]['questionId']).toEqual('1');
+      expect(EngineNameSpace['listOfChoices'][0]['roleId']).toEqual('1');
+      expect(EngineNameSpace['listOfChoices'][0]['choiceText']).toEqual('You enjoy finding solutions to problems.');
     });
   });
 });
--- src/application.js
+++ src/application.js
@@ -3,15 +3,15 @@ var createQuestionTypes = function(data){
 }
 
 var createRoles= function(data){
-  utils.populateList('role', data.feed.entry); 
+  utils.populateList('role', data.feed.entry);
 }
 
 var createQuestions= function(data){
-  
+  utils.populateList('question', data.feed.entry);
 }
 
 var createChoices = function(data){
-  
+  utils.populateList('choice', data.feed.entry);
 }
 
 
