eed24076504da585d92a6915866c23d54c937406
Santu Mahapatra <santu.m@multunus.com>
2014-12-30 18:35:58 +0530

    Removing the Json handler functions from application.js.

--- src/application.js
+++ src/application.js
@@ -1,20 +1,3 @@
-var createQuestionTypes = function(data){
-  utils.populateList('questionType', data.feed.entry);
-}
-
-var createRoles= function(data){
-  utils.populateList('role', data.feed.entry);
-}
-
-var createQuestions= function(data){
-  utils.populateList('question', data.feed.entry);
-}
-
-var createChoices = function(data){
-  utils.populateList('choice', data.feed.entry);
-}
-
-
 $(document).ready( function(){
   
 });
