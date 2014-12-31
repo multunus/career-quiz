e855c29d9fcedf11b272d84725df0f1435e753d2
Santu Mahapatra <santu.m@multunus.com>
2014-12-31 11:02:45 +0530

    Adding logic for clicking start-here button.

--- spec/applicationSpec.js
+++ spec/applicationSpec.js
@@ -10,5 +10,21 @@ describe("application", function(){
     };
   });
 
+  describe("on click of start-here button", function(){
+    beforeEach(function(){
+    $(".start-container").show();
+    $(".quiz-container").hide();
+    $(".results-container").hide();
+    });
+    it("hides start container, shows quiz", function(){
+      var spyEvent = spyOnEvent('.start-here', 'click');
+      $(".start-here").click();
+      expect('click').toHaveBeenTriggeredOn('.start-here');
+      expect(spyEvent).toHaveBeenTriggered();
+      expect($(".start-container")).toBeHidden();
+      expect($(".results-container")).toBeHidden();
+      expect($(".quiz-container")).not.toBeHidden();
+    });
+  });
   
 });
--- src/application.js
+++ src/application.js
@@ -1,3 +1,7 @@
 $(document).ready( function(){
-  
+  $('.start-here').click(function(){
+    $('.start-container').hide();
+    $('.results-container').hide();
+    $('.quiz-container').show();
+  });
 });
