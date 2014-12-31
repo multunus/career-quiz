6b17cea02608563b0fa29746ba2b36aa84638b6f
Santu Mahapatra <santu.m@multunus.com>
2014-12-31 16:22:09 +0530

    Refactoring tests in quizRunnerSpec.

--- spec/quizRunnerSpec.js
+++ spec/quizRunnerSpec.js
@@ -1,5 +1,26 @@
 describe("QuizRunner", function(){
 
+  beforeEach(function() {
+    var questionType = {'pid' : '1', 'ptype' : 'Profession'};
+    var role1 = {'pid' : '1', 'questionTypeId' : '1', 'roleName': 'programmer', 'roleText': 'You are a builder by nature. You can become a great programmer.'};
+    var role2 = {'pid' : '2', 'questionTypeId' : '1', 'roleName': 'designer', 'roleText': 'You are an artist. You can become a great designer.'};
+    var question1 = {'pid' : '1', 'questionTypeId' : '1', 'questionText': 'What makes you lose track of time?'};
+    var question2 = {'pid' : '2', 'questionTypeId' : '1', 'questionText': 'What are you curious about learning?'};
+    var choice1 = {'pid': '1', 'questionId' : '1', 'roleId': '1', 'choiceText': 'Solving a puzzle.'};
+    var choice2 = {'pid': '2', 'questionId' : '1', 'roleId': '2', 'choiceText': 'Designing something beautiful.'};
+    var choice3 = {'pid': '3', 'questionId' : '2', 'roleId': '1', 'choiceText': 'New programming techniques.'};
+    var choice4 = {'pid': '4', 'questionId' : '2', 'roleId': '2', 'choiceText': 'New design techniques.'};
+    
+    EngineNameSpace = {
+      listOfQuestionTypes : [questionType],
+      listOfQuestions : [question1, question2],
+      listOfChoices : [choice1, choice2, choice3, choice4],
+      listOfRoles : [role1, role2],
+      listOfChosenChoices : [],
+      currentQuestion: 0
+    };
+  });
+
   describe("getRadioOptionContainer", function(){
     it("creates radio option according to the group", function(){
       var question_id = '1';
@@ -11,78 +32,34 @@ describe("QuizRunner", function(){
   });
 
   describe("getChoicesForQuestion", function(){
-     beforeEach(function() {
-      var choice1 = {'questionId' : '1'};
-      var choice2 = {'questionId' : '1'};
-      EngineNameSpace = {
-        listOfChoices : [choice1, choice2]
-      };
-    });
-
     it("gets choices from a question", function(){
       var question_id = '1';
-      expect(QuizRunner.getChoicesForQuestion(question_id)).toEqual([{'questionId' : '1'}, {'questionId' : '1'}]);
+      expect(QuizRunner.getChoicesForQuestion(question_id)).toEqual([{ pid: '1', questionId: '1', roleId: '1', choiceText: 'Solving a puzzle.' }, { pid: '2', questionId: '1', roleId: '2', choiceText: 'Designing something beautiful.' }]);
     });
   });
 
   describe("getElementFromListById", function(){
-    beforeEach(function(){
-      var question1 = {'pid' : '1', 'questionText': 'What makes you lose track of time?'};
-      var question2 = {'pid' : '2', 'questionText': 'What are you curious about learning?'};
-      EngineNameSpace = {
-        listOfQuestions : [question1, question2]
-      };
-    });
     it("gets the question given a specific id", function(){
-      expect(QuizRunner.getElementFromListById(EngineNameSpace['listOfQuestions'], '1')).toEqual({'pid' : '1', 'questionText': 'What makes you lose track of time?'});
+      expect(QuizRunner.getElementFromListById(EngineNameSpace['listOfQuestions'], '1')).toEqual({ pid: '1', questionTypeId: '1', questionText: 'What makes you lose track of time?' });
     });
   });
 
   describe("fillQuestionContainer", function(){
-    beforeEach(function() {
-      var question = {'pid' : '1', 'questionText': 'What are you curious about learning?'}
-      var choice1 = {'pid': '1', 'questionId' : '1', 'choiceText': 'New programming techniques.'};
-      var choice2 ={'pid': '2', 'questionId' : '1', 'choiceText': 'New design techniques.'};
-      EngineNameSpace = {
-        listOfQuestions : [question],
-        listOfChoices : [choice1, choice2]
-      };
-    });
     it("fills the questionnaire container with the choices", function(){
       QuizRunner.fillQuestionContainer('1');
-      expect('#question-text').toHaveText('What are you curious about learning?');
+      expect('#question-text').toHaveText('What makes you lose track of time?');
       expect('#choices').toContainElement('div input');
     });
   });
 
   describe("pushChosenChoice", function(){
-    beforeEach(function() {
-      var choice1 = {'pid': '1', 'questionId' : '1', 'choiceText': 'New programming techniques.'};
-      var choice2 ={'pid': '2', 'questionId' : '1', 'choiceText': 'New design techniques.'};
-      EngineNameSpace = {
-        listOfChoices : [choice1, choice2],
-        listOfChosenChoices : []
-      };
-    });
     it("pushes the selected choice to the listOfChosenChoices", function(){
       QuizRunner.pushChosenChoice('1');
-      expect(EngineNameSpace.listOfChosenChoices).toEqual([{'pid': '1', 'questionId' : '1', 'choiceText': 'New programming techniques.'}]);
+      expect(EngineNameSpace.listOfChosenChoices).toEqual([{ pid: '1', questionId: '1', roleId: '1', choiceText: 'Solving a puzzle.' }]);
     });
   });
 
   describe("showNextQuestion", function(){
-    beforeEach(function() {
-      var question1 = {'pid' : '1', 'questionText': 'What makes you lose track of time?'};
-      var question2 = {'pid' : '2', 'questionText': 'What are you curious about learning?'};
-      var choice1 = {'pid': '1', 'questionId' : '1', 'choiceText': 'Solving a puzzle.'};
-      var choice2 = {'pid': '2', 'questionId' : '1', 'choiceText': 'Designing something beautiful.'};
-      var choice3 = {'pid': '3', 'questionId' : '2', 'choiceText': 'New programming techniques.'};
-      var choice4 = {'pid': '4', 'questionId' : '2', 'choiceText': 'New design techniques.'};
-      EngineNameSpace = {
-        listOfQuestions : [question1, question2],
-        listOfChoices : [choice1, choice2, choice3, choice4]
-      };
-    });
     describe("if current question is not last", function(){
       beforeEach(function() {
         EngineNameSpace.currentQuestion = 0;
