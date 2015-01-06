1fdb8363bf3496d19fb51d0b3c156488c1eb072e
Manish Chiniwalar <mail@manishchiniwalar.com>
2015-01-06 15:53:52 +0530

    Refactored CSS

--- src/application.js
+++ src/application.js
@@ -1,10 +1,12 @@
 $(document).ready( function(){
-  $('#start-here').click(function(){
+  $('#start-here').click(function(event){
+    event.preventDefault();
     EngineNameSpace.currentQuestion = 0;
     QuizRunner.showNextQuestion();
   });
 
-  $('#submit-choice').click(function(){
+  $('#submit-choice').click(function(event){
+    event.preventDefault();
     var radioChecked = $('#questionnaire input[type=radio]:checked');
     if ( radioChecked.length === 0  ){
       $("#error-field").show();
