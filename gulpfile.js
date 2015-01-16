492c0c3293cc3d8fb05a8389e5930e50e7a6aeae
Santu Mahapatra <santu.m@multunus.com>
2015-01-16 16:15:25 +0530

    Added Gulp task for fetching Json Data.
    To avoid running node manually and having a gulp task for the same.
    We used gulp-run library for executing getJson.js.
    
    Side Effects:
    Use has to run two gulp commmands for updating the javascript files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,13 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var run = require('gulp-run');
 
-gulp.task('default', function() {
+gulp.task('fetchData', function(){
+  run('node getJson.js')
+    .exec()
+});
+
+gulp.task('concatenate', function(){
   gulp.src(['./src/fetchedJsonData.js', './src/questionType.js', './src/question.js', './src/answer.js', './src/choice.js', './src/configuration.js', './src/ObjectFactory.js', './src/utils.js', './src/quizRunner.js', './src/applicationForProduction.js'])
     .pipe(concat({ path: 'production.js', stat: { mode: 0666 }}))
     .pipe(gulp.dest('./dist'))
